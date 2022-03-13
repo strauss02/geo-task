bd83d984c8dd1156d446c9929791f02d47aa8b0a
alonbru <alon@chimu.io>
2022-03-13 07:35:55 +0200

    chimu logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ReactComponent as Logo} from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="App-logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           Best of luck on your task!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
