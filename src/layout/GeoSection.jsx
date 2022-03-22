1187b26e19ac8fbd481f67ee0a0cbaeb29397bb9
Ido Strauss <strauss02@hotmail.co.il>
2022-03-22 19:28:08 +0200

    develop geoSection

--- src/layout/GeoSection.jsx
+++ src/layout/GeoSection.jsx
@@ -1,10 +1,29 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import {
+  changeGeographicalInfo,
+  selectDashboard,
+} from '../redux/features/dashboard/dashboardSlice'
+
+import Typography from '@mui/material/Typography'
+
+import { useSelector } from 'react-redux'
 
 function GeoSection() {
+  const DashboardState = useSelector(selectDashboard)
+
   return (
     <Box height={'100%'} bgcolor={'#444'}>
-      GeoSection!
+      <Box>
+        <Typography variant="caption">
+          Latitude: {DashboardState.geographicalInfo.lat.toFixed(2)}
+          {'  '}
+        </Typography>
+
+        <Typography variant="caption">
+          Longitude: {DashboardState.geographicalInfo.long.toFixed(2)}
+        </Typography>
+      </Box>
     </Box>
   )
 }
