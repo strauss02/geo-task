83db900e77d571c81a660f1eeaa9bb332857eaee
Ido Strauss <strauss02@hotmail.co.il>
2022-03-22 15:26:41 +0200

    create basic section components

---
+++ src/layout/AuroraSection.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function AuroraSection() {
+  return (
+    <Box height={'100%'} bgcolor={'#555'}>
+      AuroraSection
+    </Box>
+  )
+}
+
+export default AuroraSection
--- src/layout/GeoSection.jsx
+++ src/layout/GeoSection.jsx
@@ -2,7 +2,11 @@ import { Box } from '@mui/material'
 import React from 'react'
 
 function GeoSection() {
-  return <Box bgcolor={'gray'}>GeoSection!</Box>
+  return (
+    <Box height={'100%'} bgcolor={'#444'}>
+      GeoSection!
+    </Box>
+  )
 }
 
 export default GeoSection
